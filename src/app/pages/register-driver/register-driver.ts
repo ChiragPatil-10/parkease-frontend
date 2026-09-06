74d7451ff78ee7f0f3e334dad31aec0b156391d9
Chirag Patil <patilchirag192k@gmail.com>
2026-09-06 20:28:49 +0530

    feat(register-driver): add static UI for driver registration screen

---
+++ src/app/pages/register-driver/register-driver.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-register-driver',
+  imports: [RouterLink],
+  templateUrl: './register-driver.html',
+  styleUrl: './register-driver.css',
+})
+export class RegisterDriver {}
